b921e6d763946ac0c16f36c274a65669cf6344ed
chowsl <chowsl@github.dev.net>
2026-03-02 16:20:14 +0800

    title changes

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -39,7 +39,7 @@ export class UploadComponent {
   defaultVideoDesc: string = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.';
   videoDesc: string = this.defaultVideoDesc;
   addDesc: string = '';
-  loadingText: string = 'loading';
+  loadingText: string = 'Loading';
   hasEmbed: boolean = true;
 
   constructor(private snackBar: MatSnackBar, private sanitizer: DomSanitizer) {}
